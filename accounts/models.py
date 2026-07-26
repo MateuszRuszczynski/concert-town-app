a37d7449408da45890291a4a02618de055340abd
Mateusz <mateusz.ruszczynski@gmail.com>
2026-07-26 16:30:08 +0200

    fix: code refactor

--- accounts/models.py
+++ accounts/models.py
@@ -21,14 +21,14 @@ class UserManager(BaseUserManager):
         extra_fields.setdefault("is_staff", False)
         extra_fields.setdefault("is_superuser", False)
         extra_fields.setdefault("is_active", True)
-        extra_fields.setdefault("role", User.Role.CUSTOMER)
+        extra_fields.setdefault("role", self.model.Role.CUSTOMER)
         return self._create_user(email, password, **extra_fields)
 
     def create_superuser(self, email, password=None, **extra_fields):
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
         extra_fields.setdefault("is_active", True)
-        extra_fields.setdefault("role", User.Role.ADMIN)
+        extra_fields.setdefault("role", self.model.Role.ADMIN)
 
         if extra_fields.get("is_staff") is not True:
             raise ValueError("Superuser must have is_staff=True")
