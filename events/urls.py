7fce0a44bbc59668cf0c2022017654e9ab3146c4
Mateusz <mateusz.ruszczynski@gmail.com>
2026-08-10 18:46:41 +0200

    fix: solve issue with urls

--- events/urls.py
+++ events/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 from events.views import EventListCreateView
+from bookings.views import EventParticipantListView
 
 urlpatterns = [
-    path('events/', EventListCreateView.as_view(), name="event-list"),
+    path("", EventListCreateView.as_view(), name="event-list"),
+    path(
+        "event/<int:event_id>/participants/",
+        EventParticipantListView.as_view(),
+        name="registration-participants",
+    ),
 ]
